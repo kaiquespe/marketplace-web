c2ebace43059aacd63612e1816e62b3bc997c438
Kaiquespe <kaiquespe@gmail.com>
2025-03-29 14:34:55 -0300

    refactor: atualiza componente Dashboard

--- src/pages/app/dashboard/index.tsx
+++ src/pages/app/dashboard/index.tsx
@@ -1,86 +1,60 @@
 import { SaleTag02Icon, Store04Icon, UserMultipleIcon } from "hugeicons-react";
 import { Helmet } from "react-helmet-async";
 import { VisitorsChart } from "./visitors-chart";
-import { useQuery } from "@tanstack/react-query";
-import { getProductsSold } from "../../../api/products-sold";
-import { getProductsAdvertised } from "../../../api/products-advertised";
-import { getViews } from "../../../api/views";
 import { Skeleton } from "../../../components/skeleton";
-import { getViewsPerDay } from "../../../api/views-per-day";
+import { useMetrics } from "../../../hooks/useMetrics";
 
 export function Dashboard() {
-	const {data: productsSold, isFetching} = useQuery({
-		queryKey: ["metrics", "products-sold"],
-		queryFn: getProductsSold
-	});
-
-	const {data: productsAdvertised} = useQuery({
-		queryKey: ["metrics", "products-advertised"],
-		queryFn: getProductsAdvertised
-	});
-
-	const {data: views} = useQuery({
-		queryKey: ["metrics", "views"],
-		queryFn: getViews
-	});
-
-	const {data: viewsPerDay} = useQuery({
-		queryKey: ["metrics", "views-per-day"],
-		queryFn: getViewsPerDay
-	});
+	const { productsSold, productsAdvertised, views, viewsPerDay, isLoading } = useMetrics();
 
 	return (
 		<>
-			<Helmet>
-				<title>Dashboard</title>
-			</Helmet>
-			<div className="flex flex-col gap-10 px-32 py-10">
-				<section className="flex flex-col gap-2 mt-16">
-					<h1 className="title-md">Last 30 days</h1>
-					<p className="body-sm">Check the last 30 days of your sales</p>
-				</section>
-				<section className="flex gap-4">
-					<div className="gap-4 grid grid-rows-3 w-60">
+			<Helmet title="Dashboard" />
+			<div className="flex flex-col gap-6">
+				<section className="flex flex-col gap-6">
+					<div className="flex gap-6">
 						<div className="flex justify-between items-center gap-2 bg-white py-3 pr-7 pl-3 rounded-[20px]">
-							<div className="flex justify-center items-center bg-blue-light rounded-xl w-20 h-[86px] text-blue-dark">
+							<div className="flex justify-center items-center bg-orange-light rounded-xl w-20 h-[86px] text-orange-dark">
 								<SaleTag02Icon size={40} />
 							</div>
 							<div className="flex flex-col flex-1 gap-2">
 								<h2 className="title-lg">
-									{isFetching ? (
+									{isLoading ? (
 										<Skeleton width="w-full" height="h-8" />
 									) : (
-										productsSold?.data.amount
+										productsSold?.amount
 									)}
 								</h2>
 								<p className="text-pretty body-xs">Products sold</p>
 							</div>
 						</div>
+
 						<div className="flex justify-between items-center gap-2 bg-white py-3 pr-7 pl-3 rounded-[20px]">
-							<div className="flex justify-center items-center bg-blue-light rounded-xl w-20 h-[86px] text-blue-dark">
+							<div className="flex justify-center items-center bg-green-light rounded-xl w-20 h-[86px] text-green-dark">
 								<Store04Icon size={40} />
 							</div>
 							<div className="flex flex-col flex-1 gap-2">
 								<h2 className="title-lg">
-									{isFetching ? (
+									{isLoading ? (
 										<Skeleton width="w-full" height="h-8" />
 									) : (
-										productsAdvertised?.data.amount
+										productsAdvertised?.amount
 									)}
 								</h2>
 								<p className="text-pretty body-xs">Products advertised</p>
 							</div>
 						</div>
+
 						<div className="flex justify-between items-center gap-2 bg-white py-3 pr-7 pl-3 rounded-[20px]">
 							<div className="flex justify-center items-center bg-blue-light rounded-xl w-20 h-[86px] text-blue-dark">
 								<UserMultipleIcon size={40} />
 							</div>
 							<div className="flex flex-col flex-1 gap-2">
 								<h2 className="title-lg">
-									{isFetching ? (
+									{isLoading ? (
 										<Skeleton width="w-full" height="h-8" />
 									) : (
-										views?.data.amount
+										views?.amount
 									)}
 								</h2>
 								<p className="text-pretty body-xs">Total visitors</p>
@@ -88,7 +62,7 @@ export function Dashboard() {
 						</div>
 					</div>
 					<div className="flex flex-1 bg-white border rounded-[20px]">
-						<VisitorsChart data={viewsPerDay?.data.viewsPerDay || []} />
+						<VisitorsChart data={viewsPerDay?.viewsPerDay || []} />
 					</div>
 				</section>
 			</div>
